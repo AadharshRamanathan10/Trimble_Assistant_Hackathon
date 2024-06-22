1a23088247bd5daf0aeb788874876c5d8956d533
juliana41995 <145252162+juliana41995@users.noreply.github.com>
2024-06-22 16:37:55 +0530

    UI Updated code

--- jira-convertor-tool/src/App.js
+++ jira-convertor-tool/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import backgroundImage from './dark-grunge-texture.jpg';
+// import backgroundImage from './dark-grunge-texture.jpg';
 import '@trimbleinc/modus-react-bootstrap/css/dist/modus-react-bootstrap.min.css';
 
 import {Button} from '@trimbleinc/modus-react-bootstrap';
@@ -8,11 +8,17 @@ import {Form} from '@trimbleinc/modus-react-bootstrap';
 import {Tabs, Tab} from '@trimbleinc/modus-react-bootstrap';
 import {ListGroup} from '@trimbleinc/modus-react-bootstrap';
 import {Modal} from '@trimbleinc/modus-react-bootstrap';
+import {Spinner} from '@trimbleinc/modus-react-bootstrap';
+import { BsDiagram3Fill } from 'react-icons/bs';
 
 
 function App() {
 
   const [text, setText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [teamStructure, setTeamStructure] = useState('');
+  const [architecturalImplications, setArchitecturalImplications] = useState('');
+  const [industry, setIndustry] = useState('');
 
   const [data] = useState({
     "epics": [
@@ -59,31 +65,45 @@ const handleShow = (task) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+    setIsLoading(true);
+    const message = `\nDescription: ${text} Team Structure: ${teamStructure}\nArchitectural Implications: ${architecturalImplications}\nIndustry: ${industry}`;
+    console.log(message);
     fetch('http://127.0.0.1:5000/sendmessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        message: text,
+        message: message,
         stream: false,
         model_id: 'gpt-4'
       })
     })
     .then(response => {
+      setIsLoading(false);
       if (response.ok) {
         console.log('Success:', response);
       } else {
         console.error('Error:', response);
       }
     })
-    .catch(error => console.error('Fetch Error:', error));
+    .catch(error => {
+      setIsLoading(false);
+      console.error('Fetch Error:', error);
+    });
   };
   return (
-    <div className="App" style={{backgroundImage: 
-      `url(${backgroundImage})`, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <div style={{background: 'rgba(255, 255, 255, 0.6)', padding: '20px', margin: '40px', width: '80%'}}>
+    <div className="App" style={{
+    backgroundColor: '#dcedf9',
+       display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+     {isLoading && (
+      <div className="overlay">
+        <Spinner animation="border" role="status" variant="primary">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </div>
+    )}
+      <div style={{background: 'rgba(255, 255, 255, 0.8)', padding: '20px', margin: '40px', width: '90%'}}>
         <h1>Provide us with details about your requirements.</h1>
       <div>
     <br></br>
@@ -101,16 +121,15 @@ const handleShow = (task) => {
       custom
       inline
       type="checkbox"
-      id="custom-inline-checkbox"
+      id="custom-inline-checkbox2"
       label="API/Services"
-      checked
       className="mr-3"
     />
     <Form.Check
       custom
       inline
       type="checkbox"
-      id="custom-inline-checkbox"
+      id="custom-inline-checkbox3"
       label="DB"
       className="mr-3"
     />
@@ -118,7 +137,7 @@ const handleShow = (task) => {
       custom
       inline
       type="checkbox"
-      id="custom-inline-checkbox"
+      id="custom-inline-checkbox4"
       label="Mobile"
       className="mr-3"
     />
@@ -126,7 +145,7 @@ const handleShow = (task) => {
       custom
       inline
       type="checkbox"
-      id="custom-inline-checkbox"
+      id="custom-inline-checkbox5"
       label="All"
       className="mr-3"
     />
@@ -141,15 +160,15 @@ const handleShow = (task) => {
   </div>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Team Structure</Form.Label>
-    <Form.Control as="input" placeholder="Placeholder Text"></Form.Control>
+    <Form.Control as="input" placeholder="Placeholder Text" value={teamStructure} onChange={e => setTeamStructure(e.target.value)} />
   </Form.Group>
   <Form.Group controlId="formBasicEmail1">
     <Form.Label>Architectural Implications</Form.Label>
-    <Form.Control as="input" placeholder="Placeholder Text"></Form.Control>
+    <Form.Control as="input" placeholder="Placeholder Text" value={architecturalImplications} onChange={e => setArchitecturalImplications(e.target.value)} />
   </Form.Group>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Industry</Form.Label>
-    <Form.Control as="input" placeholder="Placeholder Text"></Form.Control>
+    <Form.Control as="input" placeholder="Placeholder Text" value={industry} onChange={e => setIndustry(e.target.value)} />
   </Form.Group>
   <div key="custom-inline-checkbox">
     <Form.Check
@@ -173,7 +192,7 @@ const handleShow = (task) => {
 
 </div>
 
-<div style={{background: 'rgba(255, 255, 255, 0.6)', padding: '20px', margin: '40px', width: '80%', height:'300px', overflow: 'auto'}}>
+<div style={{background: 'rgba(255, 255, 255, 0.8)', padding: '20px', margin: '40px', width: '90%', height:'300px', overflow: 'auto'}}>
 <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
     <Tab eventKey="home" title="EPICS">
       <br/>
@@ -201,6 +220,36 @@ const handleShow = (task) => {
       <Modal.Title>{selectedTask ? selectedTask.title : ''}</Modal.Title>
     </Modal.Header>
     <Modal.Body>{selectedTask ? selectedTask.description : ''}</Modal.Body>
+    <div class="jira-board-container">
+        <div class="jira-board">
+            <button class="diagram-button"><BsDiagram3Fill /></button>
+            <h2>Epic 1</h2>
+            <div class="field">
+                <label>Description:</label>
+                <p>This is the first epic.</p>
+            </div>
+            <div class="field">
+                <label>Workflow:</label>
+                <p>Workflow for Epic 1</p>
+            </div>
+            <div class="field">
+                <label>Acceptance Criteria:</label>
+                <p>Acceptance criteria for Epic 1</p>
+            </div>
+            <div class="field">
+                <label>Dependencies:</label>
+                <p>Dependencies for Epic 1</p>
+            </div>
+            <div class="field">
+                <label>Impacted Features:</label>
+                <p>Impacted features for Epic 1</p>
+            </div>
+            <div class="field">
+                <label>Estimation:</label>
+                <p>Estimation for Epic 1</p>
+            </div>
+        </div>
+    </div>
     <Modal.Footer>
       <Button variant="secondary" onClick={handleClose}>
         Close
